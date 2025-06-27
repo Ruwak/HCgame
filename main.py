0715f820111dbd448cca01ea9cb5d4ef31134c5f
Ruwak <150847546+Ruwak@users.noreply.github.com>
2025-06-26 23:26:36 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+import sys
+import os
+
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+pygame.init()
+
+# Screen
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+clock = pygame.time.Clock()
+FPS = 60
+
+running = True
+while running:
+
+    pygame.display.update()
+
+pygame.quit()
